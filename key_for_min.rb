a677cb30c38834a36e913f657c2f0d0d1eef9df5
William Mittard <billymittard@ymail.com>
2020-04-16 19:44:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,14 +3,14 @@ def key_for_min_value(hash)
     return nil
   else
     array = []
-    name_hash.each do |key, value|
+    hash.each do |key, value|
       array << value
     end
     min_num = array[0]
     array.each do |value|
       min_num = value if value < min_num
     end
-    name_hash.each do |key, value|
+    hash.each do |key, value|
       if min_num == value
         return key
       end
