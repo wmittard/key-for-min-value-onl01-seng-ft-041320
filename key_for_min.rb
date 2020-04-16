28f9d574c60fc240829db11afcdeeefbc6d6dcd5
William Mittard <billymittard@ymail.com>
2020-04-16 19:37:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,6 @@ key_for_min_value = Hash.new
 ikea = {:chair => 25, :table => 85, :mattress => 450}
 
 def key_for_min_value(ikea)
-key_for_min_value.collect do |key_name|
+key_for_min_value.collect do |key, value|
+  Key
 end
